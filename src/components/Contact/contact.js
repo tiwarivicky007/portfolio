d680ac16f478e662ff5f6a6a06066dfb2476d842
tiwarivicky007 <133739392+tiwarivicky007@users.noreply.github.com>
2024-03-15 00:52:58 +0530

    final project

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React,{useRef} from 'react';
 import './contact.css';
 import FacebookIcon from '../../assets/facebook-icon.png';
 import TwitterIcon from '../../assets/twitter.png';
@@ -7,20 +7,20 @@ import InstagramIcon from '../../assets/instagram.png';
 import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-  const form = useRef;
+  const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_bdpvbyf', 'template_s15tt96', form.current, '8GjYmQhhzZ0gsmjG8Y7Il')
+    emailjs.sendForm('service_bdpvbyf', 'template_s15tt96', form.current, 'S02lFJlsFNZFeKFJV')
       .then((result) => {
           console.log(result.text);
-          alert('Email sent!')
-          e.target.reset();
+         e.target.reset(); 
+         alert('Email sent!')
+          
         },(error) => {
           console.log(error.text);
         });
   };
-
 
   return (
     <section id="contactPage" >
@@ -28,11 +28,14 @@ const Contact = () => {
       <h1 className="contactPageTitle">Contact Me</h1>
       <span className="contactDesc">Please fill out the form below for any work opportunities</span>
      </div>
-     <form className='contactForm' ref={form} onSubmit={sendEmail}>
+     
+     <form className='contactForm'ref={form} onSubmit={sendEmail}>
+      
       <input type="text" className="name" placeholder='Your Name' name='your_name'/>
       <input type="email" className="email" placeholder='Your Email' name='your_email' />
       <textarea className='msg' name="message" rows="5" placeholder='Your Message'></textarea>
       <button type="submit" value='send' className="submitBtn">Submit</button>
+      
       <div className="links">
         <img src={FacebookIcon} alt="facebook" className='link' />
         <img src={TwitterIcon} alt="twitter" className='link' />
